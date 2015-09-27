47099085873462a932a67b419a93bbb3832c97e5
Andrey <free_apples@ymail.com>
2015-09-27 23:31:44 +0600

    Testing callback

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ var BegetAPI = new require('./beget');
 
 var beget = new BegetAPI('somna', 'zSJbnFrj');
 
-beget.user.getAccountInfo();
-beget.ftp.getList();
+beget.user.getAccountInfo(function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+        console.log(body);
+    }
+});
+beget.ftp.getList(function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+        console.log(body);
+    }
+});
