4fc1fd0266fc0d3bb6ca5e4fa687f227c2178c9d
Andrey <free_apples@ymail.com>
2015-09-27 22:38:06 +0600

    Add user methods

--- beget.js
+++ beget.js
@@ -4,69 +4,37 @@
 var request = require('request');
 var util = require('util');
 
+
+
 module.exports = function BegetAPI(login, password, params) {
-    var _baseUrl;
-    var _outputFormat;
-    var _inputFormat;
-
-    if (params === undefined) {
-        _baseUrl = 'https://api.beget.ru/api/';
-        _outputFormat = 'json';
-        _inputFormat = 'json';
-    }
-    else {
-        /** @namespace params.baseUrl */
-        _baseUrl = params.baseUrl || 'https://api.beget.ru/api/';
-        /** @namespace params.outputFormat */
-        _outputFormat = params.outputFormat || 'json';
-        /** @namespace params.inputFormat */
-       _inputFormat = params.inputFormat || 'json';
-    }
-
-    var HTTP_METHOD = 'POST';
 
     /** @namespace params.login */
     var _login = login;
+
     /** @namespace params.password */
     var _password = password;
 
-    var _path = '';
-    var _methodName = '';
-    var _inputData = {};
+    var _params = {} || params;
+    _params.login = _login;
+    _params.password = _password;
 
-    // public methods
-    this.getAccountInfo = function () {
-        _methodName = 'getAccountInfo';
-        _path = 'user/' + _methodName;
-        sendRequest();
-    };
+    var user = new require('./lib/user')(_params);
+    var backup = new require('./lib/backup')(_params);
+    var cron = new require('./lib/cron')(_params);
+    var dns = new require('./lib/dns')(_params);
+    var ftp = new require('./lib/ftp')(_params);
+    var mysql = new require('./lib/mysql')(_params);
+    var site = new require('./lib/site')(_params);
+    var domain = new require('./lib/domain')(_params);
+    var mail = new require('./lib/mail')(_params);
+    var stat = new require('./lib/stat')(_params);
 
-    // private methods
-    var sendRequest = function (params) {
-        var _response;
-        var _error;
-        var _body;
+    /*  PUBLIC METHODS  */
 
-        var _params = params || {};
-        _params.login = _login;
-        _params.passwd = _password;
-        _params.output_format = _outputFormat;
-        _params.input_format = _inputFormat;
-        _params.input_data = util.format("%j", _inputData);
+    /** user */
+    this.getAccountInfo = user.getAccountInfo;
+    this.toggleSsh = user.toggleSsh;
 
-        request({
-            uri: _path,
-            baseUrl: _baseUrl,
-            method: HTTP_METHOD,
-            qs: _params
-        }, function (error, response, body) {
-
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-            }
-        });
-
-    };
 
     return this;
 
